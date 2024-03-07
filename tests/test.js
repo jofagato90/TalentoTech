827c140407356f01a1c01faf7283d37e652786d7
jofagato <jonathan.garzon@agencialogistica.gov.co>
2024-03-06 22:44:48 -0500

    Test House Full

--- tests/test.js
+++ tests/test.js
@@ -10,7 +10,22 @@ const objecToTest = {
 
 }
 
+const objecToTest1 = {
+    "code": "12344444414",
+    "address":"cll 5-57",
+    "city": "Agua de Dios",
+    "state":"Cundinamarca",
+    "size": 150,
+    "type": "apto",
+    "price":"160000000"
+    
+
+}
+
+let Housecode;
+
 let userId;
+let houseId
 let token;
 
 
@@ -132,6 +147,56 @@ describe('POST /delete', () => {
 })
 
 
+
+
+describe('POST /house', () => {
+    it('create a new post test ', async () => {
+
+        const response = await request(app).post('/house').send(objecToTest1)
+        
+        Housecode = response.body._id;
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toHaveProperty('_id')
+        expect(response.body.address).toBe(objecToTest1.address)
+        expect(response.body.city).toBe(objecToTest1.city)
+        expect(response.body.state).toBe(objecToTest1.state)
+        expect(response.body.size).toBe(objecToTest1.size)
+        expect(response.body.type).toBe(objecToTest1.type)
+        expect(response.body.price).toBe(objecToTest1.price)
+        expect(response.body.code).toBe(objecToTest1.code)
+    })
+
+})
+
+describe('GET /house/:code', () => {
+    it('responds with an Object that contains an specific House', async () => {
+        
+        const response = await request(app).get('/house/' + Housecode);
+        Housecode = response.body.code;
+        expect(response.statusCode).toBe(200)
+        expect(typeof response.body === "object").toBe(true);
+        expect(response.body).toHaveProperty('_id')
+        expect(response.body.address).toBe(objecToTest1.address)
+        expect(response.body.city).toBe(objecToTest1.city)
+        expect(response.body.state).toBe(objecToTest1.state)
+        expect(response.body.size).toBe(objecToTest1.size)
+        expect(response.body.type).toBe(objecToTest1.type)
+        expect(response.body.price).toBe(objecToTest1.price)
+        expect(response.body.code).toBe(objecToTest1.code)
+    })
+})
+
+
+describe('/house/:code', () => {
+    it('Success delete with _id', async () => { 
+             
+        const response = await request(app).delete('/house/'+ Housecode)
+        Housecode = response.body.code;  
+        expect(response.statusCode).toBe(200)
+        expect(response.body.status).toBe("success")
+    })
+})
 
 
 
